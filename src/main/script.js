c9ad6c9cbd61ffbae9f543e1abdbf0a77981bcba
TsvetomirTsvetkov <cvetomircvetkov24@gmail.com>
2023-12-21 06:30:48 +0200

    Update testcode

--- src/main/script.js
+++ src/main/script.js
@@ -7,14 +7,46 @@ document.getElementById('registrationForm').addEventListener('submit', function(
         event.preventDefault();
       }
 
+      if (password.length == 0){
+        alert("Password cannot be empty");
+        event.preventDefault();
+      }
+
       if (password.length < 8) {
         alert("Password must be at least 8 characters long");
+        event.preventDefault();
+      }
+
+      if (password.length > 30) {
+        alert("Password must be at most 30 characters long");
         event.preventDefault();
       }
     });
 
     function validateEmail(email) {
       // Basic email validation with regular expression
-      var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-      return re.test(email);
+      if (email.includes('@') && email.includes('.')) {
+        var atIndex = email.indexOf('@');
+        var dotIndex = email.lastIndexOf('.');
+        var spaceIndex = email.indexOf(' ');
+
+        // Check if '@' appears before '.'
+        if (atIndex < dotIndex) {
+          // Check if there's only one '@'
+          if (email.indexOf('@', atIndex + 1) === -1) {
+            // Check if there's no space in the email
+            if (spaceIndex === -1) {
+              return "Valid email";
+            } else {
+              return "Invalid email: Contains space";
+            }
+          } else {
+            return "Invalid email: Multiple '@' symbols";
+          }
+        } else {
+          return "Invalid email: '.' appears before '@'";
+        }
+    } else {
+      return "Invalid email: Missing '@' or '.'";
+    }
     }
--- src/tests/register/test_registration_form.py
+++ src/tests/register/test_registration_form.py
@@ -18,26 +18,98 @@ class TestRegistrationForm(unittest.TestCase):
         # Close the browser window
         self.driver.quit()
 
-    def test_registration_form_fail_without_password(self):
+    def test_registration_form_success(self):
         # Get fields
         email = self.driver.find_element(By.ID, 'email')
+        password = self.driver.find_element(By.ID, 'password')
         submit = self.driver.find_element(By.ID, 'submit')
 
         # Fill in the form fields
-        email.send_keys('test@test.com')
+        email.send_keys('example@example.com')
+        password.send_keys('password123')
+
+        # Get the initial URL
+        initial_url = self.driver.current_url
 
         # Click the submit button
         submit.click()
         time.sleep(1)
 
-        # Wait for the "Thank You" container to be visible
-        # try:
-        #     thank_you_container = WebDriverWait(self.driver, 3).until(
-        #         EC.presence_of_element_located((By.ID, 'thankYouContainer'))
-        #     )
-        #     self.assertTrue(thank_you_container.is_displayed(), "Thank You container is visible!")
-        # except Exception as e:
-        #     self.fail(f"Thank You container is not visible. Exception: {e}")
+        # Assert that the current URL after submission is the same as the initial URL
+        current_url = self.driver.current_url
+        self.assertEqual(initial_url, current_url, "Redirected to the same page after form submission")
+
+    def test_registration_form_password_short(self):
+        # Get fields
+        email = self.driver.find_element(By.ID, 'email')
+        password = self.driver.find_element(By.ID, 'password')
+        submit = self.driver.find_element(By.ID, 'submit')
+
+        # Fill in the form fields
+        email.send_keys('example@example.com')
+        password.send_keys('1234')
+
+        # Click the submit button
+        submit.click()
+        time.sleep(1)
+
+        # Check for an alert message
+        try:
+            alert = self.driver.switch_to.alert
+            self.assertIsNotNone(alert, "Alert not found")
+            alert_text = alert.text
+            self.assertEqual(alert_text, "Password must be at least 8 characters long", "Unexpected alert message")
+            alert.accept()
+        except Exception:
+            self.fail("No alert detected for missing password")
+
+    def test_registration_form_password_long(self):
+        # Get fields
+        email = self.driver.find_element(By.ID, 'email')
+        password = self.driver.find_element(By.ID, 'password')
+        submit = self.driver.find_element(By.ID, 'submit')
+
+        # Fill in the form fields
+        email.send_keys('example@example.com')
+        password.send_keys('123456789123456789123456789123456789')
+
+        # Click the submit button
+        submit.click()
+        time.sleep(1)
+
+        # Check for an alert message
+        try:
+            alert = self.driver.switch_to.alert
+            self.assertIsNotNone(alert, "Alert not found")
+            alert_text = alert.text
+            self.assertEqual(alert_text, "Password must be at most 30 characters long", "Unexpected alert message")
+            alert.accept()
+        except Exception:
+            self.fail("No alert detected for missing password")
+
+    def test_registration_form_password_empty(self):
+        # Get fields
+        email = self.driver.find_element(By.ID, 'email')
+        password = self.driver.find_element(By.ID, 'password')
+        submit = self.driver.find_element(By.ID, 'submit')
+
+        # Fill in the form fields
+        email.send_keys('example@example.com')
+        password.send_keys('')
+
+        # Click the submit button
+        submit.click()
+        time.sleep(1)
+
+        # Check for an alert message
+        try:
+            alert = self.driver.switch_to.alert
+            self.assertIsNotNone(alert, "Alert not found")
+            alert_text = alert.text
+            self.assertEqual(alert_text, "Password cannot be empty", "Unexpected alert message")
+            alert.accept()
+        except Exception:
+            self.fail("No alert detected for missing password")
 
 
 if __name__ == '__main__':
