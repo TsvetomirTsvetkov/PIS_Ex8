da58e08e8f5017c7966eee9e07585f1f3ca35236
TsvetomirTsvetkov <cvetomircvetkov24@gmail.com>
2023-12-21 09:41:51 +0200

    Update html, js and tests

--- src/main/script.js
+++ src/main/script.js
@@ -1,52 +1,53 @@
 document.getElementById('registrationForm').addEventListener('submit', function(event) {
-      var email = document.getElementById('email').value;
-      var password = document.getElementById('password').value;
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
 
-      if (!validateEmail(email)) {
-        alert("Please enter a valid email address");
-        event.preventDefault();
+    // #############################
+    // Checks for Email            #
+    // #############################
+
+    if (email.length == 0){
+      alert("Email cannot be empty");
+      event.preventDefault();
+    }
+
+    if (email.includes('@') && email.includes('.')) {
+      var atIndex = email.indexOf('@');
+      var dotIndex = email.lastIndexOf('.');
+
+      if (atIndex < dotIndex) {
+          if (email.indexOf('@', atIndex + 1) === -1) {
+              if (email.includes(' ')) {
+                  alert("Invalid email: Contains space");
+              }
+          } else {
+              alert("Invalid email: Multiple '@' symbols");
+          }
+      } else {
+          alert("Invalid email: '.' appears before '@'");
       }
 
-      if (password.length == 0){
+    } else {
+        alert("Invalid email: Missing '@' or '.'");
+        event.preventDefault();
+    }
+
+    // #############################
+    // Checks for Password         #
+    // #############################
+
+    if (password.length == 0){
         alert("Password cannot be empty");
         event.preventDefault();
-      }
+    }
 
-      if (password.length < 8) {
+    if (password.length < 8) {
         alert("Password must be at least 8 characters long");
         event.preventDefault();
-      }
+    }
 
-      if (password.length > 30) {
+    if (password.length > 30) {
         alert("Password must be at most 30 characters long");
         event.preventDefault();
-      }
-    });
-
-    function validateEmail(email) {
-      // Basic email validation with regular expression
-      if (email.includes('@') && email.includes('.')) {
-        var atIndex = email.indexOf('@');
-        var dotIndex = email.lastIndexOf('.');
-        var spaceIndex = email.indexOf(' ');
-
-        // Check if '@' appears before '.'
-        if (atIndex < dotIndex) {
-          // Check if there's only one '@'
-          if (email.indexOf('@', atIndex + 1) === -1) {
-            // Check if there's no space in the email
-            if (spaceIndex === -1) {
-              return "Valid email";
-            } else {
-              return "Invalid email: Contains space";
-            }
-          } else {
-            return "Invalid email: Multiple '@' symbols";
-          }
-        } else {
-          return "Invalid email: '.' appears before '@'";
-        }
-    } else {
-      return "Invalid email: Missing '@' or '.'";
     }
-    }
+});
--- src/tests/register/test_registration_form.py
+++ src/tests/register/test_registration_form.py
@@ -61,7 +61,7 @@ class TestRegistrationForm(unittest.TestCase):
             self.assertEqual(alert_text, "Password must be at least 8 characters long", "Unexpected alert message")
             alert.accept()
         except Exception:
-            self.fail("No alert detected for missing password")
+            self.fail("No alert detected for password shorter than expected")
 
     def test_registration_form_password_long(self):
         # Get fields
@@ -85,7 +85,7 @@ class TestRegistrationForm(unittest.TestCase):
             self.assertEqual(alert_text, "Password must be at most 30 characters long", "Unexpected alert message")
             alert.accept()
         except Exception:
-            self.fail("No alert detected for missing password")
+            self.fail("No alert detected for password longer than expected")
 
     def test_registration_form_password_empty(self):
         # Get fields
@@ -110,6 +110,150 @@ class TestRegistrationForm(unittest.TestCase):
             alert.accept()
         except Exception:
             self.fail("No alert detected for missing password")
+
+    def test_registration_form_email_empty(self):
+        # Get fields
+        email = self.driver.find_element(By.ID, 'email')
+        password = self.driver.find_element(By.ID, 'password')
+        submit = self.driver.find_element(By.ID, 'submit')
+
+        # Fill in the form fields
+        email.send_keys('')
+        password.send_keys('password123')
+
+        # Click the submit button
+        submit.click()
+        time.sleep(1)
+
+        # Check for an alert message
+        try:
+            alert = self.driver.switch_to.alert
+            self.assertIsNotNone(alert, "Alert not found")
+            alert_text = alert.text
+            self.assertEqual(alert_text, "Email cannot be empty", "Unexpected alert message")
+            alert.accept()
+        except Exception:
+            self.fail("No alert detected for missing email")
+
+    def test_registration_form_email_at_after_dot(self):
+        # Get fields
+        email = self.driver.find_element(By.ID, 'email')
+        password = self.driver.find_element(By.ID, 'password')
+        submit = self.driver.find_element(By.ID, 'submit')
+
+        # Fill in the form fields
+        email.send_keys('exampleexample.@com')
+        password.send_keys('password123')
+
+        # Click the submit button
+        submit.click()
+        time.sleep(1)
+
+        # Check for an alert message
+        try:
+            alert = self.driver.switch_to.alert
+            self.assertIsNotNone(alert, "Alert not found")
+            alert_text = alert.text
+            self.assertEqual(alert_text, "Invalid email: '.' appears before '@'", "Unexpected alert message")
+            alert.accept()
+        except Exception:
+            self.fail("No alert detected for '@' after '.'")
+
+    def test_registration_form_email_multiple_ats(self):
+        # Get fields
+        email = self.driver.find_element(By.ID, 'email')
+        password = self.driver.find_element(By.ID, 'password')
+        submit = self.driver.find_element(By.ID, 'submit')
+
+        # Fill in the form fields
+        email.send_keys('example@@example.com')
+        password.send_keys('password123')
+
+        # Click the submit button
+        submit.click()
+        time.sleep(1)
+
+        # Check for an alert message
+        try:
+            alert = self.driver.switch_to.alert
+            self.assertIsNotNone(alert, "Alert not found")
+            alert_text = alert.text
+            self.assertEqual(alert_text, "Invalid email: Multiple '@' symbols", "Unexpected alert message")
+            alert.accept()
+        except Exception:
+            self.fail("No alert detected for multiple '@' symbols")
+
+    def test_registration_form_email_missing_at(self):
+        # Get fields
+        email = self.driver.find_element(By.ID, 'email')
+        password = self.driver.find_element(By.ID, 'password')
+        submit = self.driver.find_element(By.ID, 'submit')
+
+        # Fill in the form fields
+        email.send_keys('exampleexample.com')
+        password.send_keys('password123')
+
+        # Click the submit button
+        submit.click()
+        time.sleep(1)
+
+        # Check for an alert message
+        try:
+            alert = self.driver.switch_to.alert
+            self.assertIsNotNone(alert, "Alert not found")
+            alert_text = alert.text
+            self.assertEqual(alert_text, "Invalid email: Missing '@' or '.'", "Unexpected alert message")
+            alert.accept()
+        except Exception:
+            self.fail("No alert detected for missing '@'")
+
+    def test_registration_form_email_missing_dot(self):
+        # Get fields
+        email = self.driver.find_element(By.ID, 'email')
+        password = self.driver.find_element(By.ID, 'password')
+        submit = self.driver.find_element(By.ID, 'submit')
+
+        # Fill in the form fields
+        email.send_keys('example@examplecom')
+        password.send_keys('password123')
+
+        # Click the submit button
+        submit.click()
+        time.sleep(1)
+
+        # Check for an alert message
+        try:
+            alert = self.driver.switch_to.alert
+            self.assertIsNotNone(alert, "Alert not found")
+            alert_text = alert.text
+            self.assertEqual(alert_text, "Invalid email: Missing '@' or '.'", "Unexpected alert message")
+            alert.accept()
+        except Exception:
+            self.fail("No alert detected for missing '.'")
+
+    def test_registration_form_email_space(self):
+        # Get fields
+        email = self.driver.find_element(By.ID, 'email')
+        password = self.driver.find_element(By.ID, 'password')
+        submit = self.driver.find_element(By.ID, 'submit')
+
+        # Fill in the form fields
+        email.send_keys('example@ example.com')
+        password.send_keys('password123')
+
+        # Click the submit button
+        submit.click()
+        time.sleep(1)
+
+        # Check for an alert message
+        try:
+            alert = self.driver.switch_to.alert
+            self.assertIsNotNone(alert, "Alert not found")
+            alert_text = alert.text
+            self.assertEqual(alert_text, "Invalid email: Contains space", "Unexpected alert message")
+            alert.accept()
+        except Exception:
+            self.fail("No alert detected for empty space")
 
 
 if __name__ == '__main__':
